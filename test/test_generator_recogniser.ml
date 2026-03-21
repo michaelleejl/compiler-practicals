0b677ffd2bf05a09c8753de009f856c5a356f1f8
michaelleejl <michaelleejl@gmail.com>
2026-03-21 19:56:42 +0800

    Update tests

---
+++ test/test_generator_recogniser.ml
@@ -0,0 +1,38 @@
+open Compilers.Lexing.Generators.Recogniser
+open Printf
+
+let keywords = parse "let|rec|in|fun|true|false"
+let operators = parse {|=|\+|->|(|)|}
+let ident = parse "[a-zA-Z][a-zA-Z0-9]*"
+let literal = parse "-?[0-9]+"
+let whitespace = parse {|\s|}
+
+let recognise_one =
+  keywords >| operators >| ident >| literal
+
+let mlot_recogniser = generate (epsilon >| recognise_one >& ~*(whitespace >& recognise_one))
+
+
+let%expect_test _ =
+  printf "%b" (recognise mlot_recogniser "fun");
+  [%expect {| true |}]
+
+let%expect_test _ =
+  printf "%b" (recognise mlot_recogniser "fun ->");
+  [%expect {| true |}]
+
+let%expect_test _ =
+  printf "%b" (recognise mlot_recogniser "x");
+  [%expect {| true |}]
+
+let%expect_test _ =
+  printf "%b" (recognise mlot_recogniser "-123");
+  [%expect {| true |}]
+
+let%expect_test _ =
+  printf "%b" (recognise mlot_recogniser "0123");
+  [%expect {| true |}]
+
+let%expect_test _ =
+  printf "%b" (recognise mlot_recogniser "fun->");
+  [%expect {| false |}]
