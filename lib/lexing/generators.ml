69eb2ae2393d1b4821a8077a981caa045eabcbd0
michaelleejl <michaelleejl@gmail.com>
2026-03-22 17:13:50 +0800

    Add lexer generator

--- lib/lexing/generators.ml
+++ lib/lexing/generators.ml
@@ -17,3 +17,124 @@ module Recogniser = struct
   let generate r = Regex.compile r |> Dfa.determinise
   let recognise = Dfa.accept
 end
+
+open Intfs
+open Intfs.Language
+open Intfs.Tags
+
+module Lexer (Lang : L) (Tag : T with type token = Lang.token) = struct
+  exception LexFailure of string
+
+  type token = Lang.token
+  type m = Regex.t
+
+  module TaggedNfa = Tnfa.Make (Tag)
+
+  type tag = Tag.t
+  type t = TaggedNfa.t
+
+  module Matcher = struct
+    open Regex
+
+    let empty = empty
+    let epsilon = epsilon
+    let chr = chr
+    let str = str
+    let ( >| ) = alt
+    let ( >& ) = seq
+    let ( ~* ) = kleene
+    let ( ~+ ) = plus
+    let ( ~? ) = opt
+    let parse = parse
+  end
+
+  open TaggedNfa
+
+  let create matcher tag = lift (Regex.compile matcher) tag
+  let ( >>| ) = alt
+
+  type lexing_state = {
+    state : state_set;
+    rest : char list;
+    tokens : token list;
+    buffer : char list;
+    last_accepting : (int * state_set) option;
+  }
+
+  let lex_step machine { state; rest; tokens; buffer; last_accepting } =
+    if is_rejecting machine state then
+      match last_accepting with
+      | None -> raise (LexFailure "no last accepting state")
+      | Some (k, qs) -> (
+          let tag = emit_tag machine qs in
+          match tag with
+          | None -> raise (LexFailure "tag is empty")
+          | Some tag -> (
+              let chars = List.drop k buffer in
+              let buffer = List.take k buffer in
+              let action = (Tag.tag_to_action tag) (List.rev chars) in
+              let last_accepting = None in
+              let state = initialise machine in
+              let rest = List.rev buffer @ rest in
+              let buffer = [] in
+              match action with
+              | None -> { rest; tokens; buffer; last_accepting; state }
+              | Some t ->
+                  { rest; tokens = t :: tokens; buffer; last_accepting; state })
+          )
+    else
+      match rest with
+      | [] ->
+          if is_accepting machine state then
+            let tag = emit_tag machine state in
+            match tag with
+            | None -> raise (LexFailure "tag is empty")
+            | Some tag -> (
+                let action = (Tag.tag_to_action tag) (List.rev buffer) in
+                match action with
+                | None ->
+                    { rest; tokens; buffer = []; last_accepting = None; state }
+                | Some t ->
+                    {
+                      rest;
+                      tokens = t :: tokens;
+                      buffer = [];
+                      last_accepting = None;
+                      state;
+                    })
+          else raise (LexFailure "last state was not accepting")
+      | c :: rest ->
+          let next_state = step machine state c in
+          let new_accepting =
+            if is_accepting machine next_state then Some (0, next_state)
+            else
+              match last_accepting with
+              | None -> None
+              | Some (i, qs) -> Some (i + 1, qs)
+          in
+          {
+            rest;
+            tokens;
+            buffer = c :: buffer;
+            last_accepting = new_accepting;
+            state = next_state;
+          }
+
+  let rec lex_run machine state =
+    match (state.rest, state.buffer) with
+    | [], [] -> List.rev state.tokens
+    | _, _ -> lex_run machine (lex_step machine state)
+
+  let lex machine s =
+    let cs = Base.String.to_list s in
+    let initial_state =
+      {
+        state = initialise machine;
+        rest = cs;
+        tokens = [];
+        buffer = [];
+        last_accepting = None;
+      }
+    in
+    lex_run machine initial_state
+end
--- lib/lexing/generators.mli
+++ lib/lexing/generators.mli
@@ -1,3 +1,7 @@
+open Intfs
+open Intfs.Language
+open Intfs.Tags
+
 module Recogniser : sig
   type t
   type s
@@ -14,4 +18,35 @@ module Recogniser : sig
   val parse : string -> t
   val generate : t -> s
   val recognise : s -> string -> bool
+end
+
+module Lexer (Lang : L) (Tag : T with type token = Lang.token) : sig
+  type m
+  type t
+  type tag = Tag.t
+  type token = Lang.token
+
+  module Matcher : sig
+    val empty : m
+    val epsilon : m
+    val chr : char -> m
+    val str : string -> m
+    val ( >| ) : m -> m -> m
+    val ( >& ) : m -> m -> m
+    val ( ~* ) : m -> m
+    val ( ~+ ) : m -> m
+    val ( ~? ) : m -> m
+    val parse : string -> m
+  end
+
+  val create : m -> tag -> t
+  val ( >>| ) : t -> t -> t
+  val lex : t -> string -> token list
+
+  (* 
+  
+
+  val generate : t -> s 
+
+  val lex : s -> string -> token list  *)
 end
